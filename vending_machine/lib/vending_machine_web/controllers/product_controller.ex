595642fd9983cc4856efe74248beb1bda3ffdf10
Aghiles Lounis <aghiles.lounis@polyconseil.fr>
2024-01-22 20:50:38 +0100

    create API endpoint for products

---
+++ vending_machine/lib/vending_machine_web/controllers/product_controller.ex
@@ -0,0 +1,50 @@
+defmodule VendingMachineWeb.ProductController do
+  use VendingMachineWeb, :controller
+
+  alias VendingMachine.Products
+  alias VendingMachine.Products.Product
+
+  action_fallback VendingMachineWeb.FallbackController
+
+  def index(conn, _params) do
+    products = Products.list_products()
+    render(conn, :index, products: products)
+  end
+
+  def create(conn, %{"product" => product_params}) do
+    with {:ok, %Product{} = product} <- Products.create_product(product_params) do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", ~p"/api/products/#{product}")
+      |> render(:show, product: product)
+    end
+  end
+
+  def create(_conn, invalid_params) do
+    changeset = Products.change_product(%Product{}, invalid_params)
+
+    {:error, changeset}
+  end
+
+  @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def show(conn, %{"id" => id}) do
+    product = Products.get_product!(id)
+    render(conn, :show, product: product)
+  end
+
+  def update(conn, %{"id" => id, "product" => product_params}) do
+    product = Products.get_product!(id)
+
+    with {:ok, %Product{} = product} <- Products.update_product(product, product_params) do
+      render(conn, :show, product: product)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    product = Products.get_product!(id)
+
+    with {:ok, %Product{}} <- Products.delete_product(product) do
+      send_resp(conn, :no_content, "")
+    end
+  end
+end
